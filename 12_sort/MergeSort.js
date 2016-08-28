87d8205c1bcf226d4b28ac6163e63f1d6aa687e3
kjnam <kyujnam@gmail.com>
2016-08-28 09:07:06 +0900

    add(sort): 병합정렬 추가

---
+++ 12_sort/MergeSort.js
@@ -0,0 +1,116 @@
+class MergeSort {
+    constructor(size) {
+        this.dataStore = [];
+        this.pos = 0;
+        this.size = size;
+
+        this.init();
+    }
+
+    init() {
+        for (let i=0; i<this.size; ++i) {
+            this.dataStore[i] = i;
+        }
+
+        this.shuffle();
+    }
+
+    mergeSort(arr) {
+        if (!arr) {
+            arr = this.dataStore;
+        }
+
+        if(arr.length < 2){
+            return;
+        }
+
+        console.log("=============== 병합정렬 ===============");
+        let step = 1;
+        let left, right;
+        while (step < arr.length) {
+            left = 0;
+            right = step;
+            while ((right+step) <= arr.length) {
+                this.mergeArrays(arr, left, left+step, right, right+step);
+                left = right + step;
+                right = left + step;
+            }
+            if (right < arr.length) {
+                this.mergeArrays(arr, left, left+step, right, arr.length);
+            }
+            step *= 2;
+        }
+    }
+
+    mergeArrays(arr, startLeft, stopLeft, startRight, stopRight) {
+        let rightArr = new Array(stopRight - startRight + 1);
+        let leftArr = new Array(stopLeft - startLeft + 1);
+        let k = startRight;
+        for (let i=0; i<rightArr.length-1; ++i) {
+            rightArr[i] = arr[k++];
+        }
+
+        k = startLeft;
+        for (let i=0; i<leftArr.length-1; ++i) {
+            leftArr[i] = arr[k++];
+        }
+
+        rightArr[rightArr.length-1] = Infinity;
+        leftArr[leftArr.length-1] = Infinity;
+
+        let m = 0;
+        let n = 0;
+        for (k=startLeft; k<stopRight; ++k) {
+            if (leftArr[m] <= rightArr[n]) {
+                arr[k] = leftArr[m++];
+            } else {
+                arr[k] = rightArr[n++];
+            }
+        }
+        // console.log(`left arrays - ${leftArr.toString()}`);
+        // console.log(`right arrays - ${rightArr.toString()}`);
+        // console.log(`arrays - ${arr.toString()}`);
+    }
+
+    shuffle() {
+        let seed;
+        let temp;
+
+        for (let i=0; i<this.dataStore.length; ++i) {
+            seed = Math.floor(Math.random()*(this.dataStore.length));
+            temp = this.dataStore[i];
+            this.dataStore[i] = this.dataStore[seed];
+            this.dataStore[seed] = temp;
+        }
+    }
+
+    insert(elemnet) {
+        this.dataStore[this.pos++] = elemnet;
+    }
+
+    clear() {
+        for (let i=0; i<this.size; ++i) {
+            this.dataStore[i] = 0;
+        }
+    }
+
+    swap(idx1, idx2) {
+        let temp = this.dataStore[idx1];
+        this.dataStore[idx1] = this.dataStore[idx2];
+        this.dataStore[idx2] = temp;
+    }
+
+    toString() {
+        return `[${this.dataStore.join(',')}];`
+    }
+
+    isValidSorting() {
+        for (let i=0; i<this.dataStore.length-1; ++i) {
+            if (i > i+1) {
+                console.log('MergeSort Test Faild');
+                break;
+            }
+        }
+        console.log('MergeSort Test Passed!');
+    }
+}
